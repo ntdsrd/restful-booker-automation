af69c2722bc533c5557543ef8eade547891eb719
ntdsrd <thedat0309@gmail.com>
2024-02-20 17:57:05 +0700

    feat: add get booking id method

--- src/main/java/actions/json/CreateBookingJson.java
+++ src/main/java/actions/json/CreateBookingJson.java
@@ -3,8 +3,11 @@ package actions.json;
 import actions.commons.GlobalConstants;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.json.JSONObject;
 
 public class CreateBookingJson {
+    JSONObject jsonObject;
+
     public void PostRequest(String firstname,
                             String lastname,
                             String totalprice,
@@ -12,7 +15,7 @@ public class CreateBookingJson {
                             String checkin,
                             String checkout,
                             String additionalneeds) {
-        HttpResponse<String> response = Unirest.post(GlobalConstants.load("url"))
+        HttpResponse<String> response = Unirest.post(GlobalConstants.loadProd("url"))
                 .contentType(GlobalConstants.JSON)
                 .accept(GlobalConstants.JSON)
                 .body(GlobalConstants.JSON(firstname,
@@ -23,6 +26,12 @@ public class CreateBookingJson {
                         checkout,
                         additionalneeds))
                 .asString();
-        System.out.println(response.getBody());
+        jsonObject = new JSONObject(response.getBody());
+        System.out.println("POST: " + response.getBody());
+    }
+
+    public void GetBookingId() {
+        String bookingId = jsonObject.get("bookingid").toString();
+        System.out.println("Booking ID: " + bookingId);
     }
 }
