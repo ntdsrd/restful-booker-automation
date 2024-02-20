7700e86dfc742b16a63ef9097e37d76f573c670e
ntdsrd <thedat0309@gmail.com>
2024-02-20 17:10:04 +0700

    feat: add load properties file method, json format, testng soft assert and application/json variable

---
+++ src/main/java/actions/commons/GlobalConstants.java
@@ -0,0 +1,45 @@
+package actions.commons;
+
+import org.testng.asserts.SoftAssert;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GlobalConstants {
+    public static SoftAssert softAssert = new SoftAssert();
+    public static final String JSON = "application/json";
+
+    public static String load(String data) {
+        Properties properties;
+        try {
+            InputStream inputStream = new FileInputStream("src/main/resources/data.properties");
+            properties = new Properties();
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties.getProperty(data);
+    }
+
+    public static String JSON(String firstname,
+                              String lastname,
+                              String totalprice,
+                              String depositpaid,
+                              String checkin,
+                              String checkout,
+                              String additionalneeds) {
+        return "{\n" +
+                "  \"firstname\": \"" + firstname + "\",\n" +
+                "  \"lastname\": \"" + lastname + "\",\n" +
+                "  \"totalprice\": \"" + totalprice + "\",\n" +
+                "  \"depositpaid\": " + depositpaid + ",\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"" + checkin + "\",\n" +
+                "    \"checkout\": \"" + checkout + "\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"" + additionalneeds + "\"\n" +
+                "}";
+    }
+}
