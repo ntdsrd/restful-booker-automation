f92e37e17a6bf7c8705b05e10a2dec49fdbbbc6a
ntdsrd <thedat0309@gmail.com>
2024-02-20 17:18:19 +0700

    refactor: delete PageGeneratorManager.java

--- src/main/java/actions/commons/PageGeneratorManager.java
+++
@@ -1,9 +0,0 @@
-package actions.commons;
-
-import actions.json.CreateBookingJson;
-
-public class PageGeneratorManager {
-    public static CreateBookingJson createBookingJson() {
-        return new CreateBookingJson();
-    }
-}
