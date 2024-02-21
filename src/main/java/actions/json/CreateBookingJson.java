76dbfdca6a066be4d3bdb349c619d5c97d847110
ntdsrd <thedat0309@gmail.com>
2024-02-21 16:48:34 +0700

    feat: add validate status code and handle exception for all methods

--- src/main/java/actions/json/CreateBookingJson.java
+++ src/main/java/actions/json/CreateBookingJson.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 
 public class CreateBookingJson {
     public static String bookingId;
+    HttpResponse<String> response;
     JSONObject jsonObject;
 
     public void PostRequest(String firstname,
@@ -16,23 +17,44 @@ public class CreateBookingJson {
                             String checkin,
                             String checkout,
                             String additionalneeds) {
-        HttpResponse<String> response = Unirest.post(GlobalConstants.loadProd("url"))
-                .contentType(GlobalConstants.JSON)
-                .accept(GlobalConstants.JSON)
-                .body(GlobalConstants.JSON(firstname,
-                        lastname,
-                        totalprice,
-                        depositpaid,
-                        checkin,
-                        checkout,
-                        additionalneeds))
-                .asString();
-        jsonObject = new JSONObject(response.getBody());
-        System.out.println("POST: " + response.getBody());
+        try {
+            response = Unirest.post(GlobalConstants.loadProd("url"))
+                    .contentType(GlobalConstants.JSON)
+                    .accept(GlobalConstants.JSON)
+                    .body(GlobalConstants.JSON(firstname,
+                            lastname,
+                            totalprice,
+                            depositpaid,
+                            checkin,
+                            checkout,
+                            additionalneeds))
+                    .asString();
+            jsonObject = new JSONObject(response.getBody());
+            System.out.println("POST: " + response.getBody());
+        } catch (Exception exception) {
+            throw new RuntimeException("Send POST request fail");
+        }
     }
 
     public void GetBookingId() {
-        bookingId = jsonObject.get("bookingid").toString();
-        System.out.println("Booking ID: " + bookingId);
+        try {
+            bookingId = jsonObject.get("bookingid").toString();
+            System.out.println("Booking ID: " + bookingId);
+        } catch (Exception exception) {
+            throw new RuntimeException("Get booking id fail");
+        }
+
+    }
+
+    public void ValidateStatusCode() {
+        int status;
+        try {
+            status = response.getStatus();
+            System.out.println("CODE: " + status + " " + response.getStatusText());
+        } catch (Exception exception) {
+            throw new RuntimeException("Cannot get status code");
+        }
+        GlobalConstants.softAssert.assertEquals(status, 200);
+        GlobalConstants.softAssert.assertAll("Validate status code fail");
     }
 }
