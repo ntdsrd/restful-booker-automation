4c3092b8a05967a0cb6b3cbf8ad3e5e314ccf400
ntdsrd <thedat0309@gmail.com>
2024-02-22 22:39:15 +0700

    feat: add validate first name method

--- src/main/java/actions/json/GetBookingJson.java
+++ src/main/java/actions/json/GetBookingJson.java
@@ -3,12 +3,32 @@ package actions.json;
 import actions.commons.GlobalConstants;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.json.JSONObject;
 
 public class GetBookingJson {
+    JSONObject jsonObject;
+
     public void GetRequest() {
-        HttpResponse<String> response = Unirest.get(GlobalConstants.loadProd("url").concat("/" + CreateBookingJson.bookingId))
-                .accept(GlobalConstants.JSON)
-                .asString();
-        System.out.println("GET: " + response.getBody());
+        try {
+            HttpResponse<String> response = Unirest.get(GlobalConstants.loadProperties("prod", "url").concat("/" + CreateBookingJson.bookingId))
+                    .accept(GlobalConstants.JSON)
+                    .asString();
+            jsonObject = new JSONObject(response.getBody());
+            System.out.println("GET: " + response.getBody());
+        } catch (Exception exception) {
+            throw new RuntimeException("SEND GET REQUEST FAIL");
+        }
+    }
+
+    public void ValidateFirstName(String firstName) {
+        String firstNameResponse;
+        try {
+            firstNameResponse = jsonObject.getString("firstname");
+            System.out.println("FIRST NAME: " + firstNameResponse);
+        } catch (Exception exception) {
+            throw new RuntimeException("GET FIRST NAME FAIL");
+        }
+        GlobalConstants.softAssert.assertEquals(firstNameResponse, firstName);
+        GlobalConstants.softAssert.assertAll("Validate first name fail");
     }
 }
