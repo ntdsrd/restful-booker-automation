ed5873724e516f255b524ec6ec7db8f8689a6ea0
ntdsrd <thedat0309@gmail.com>
2024-02-22 22:36:42 +0700

    refactor: change loadProd to loadProperties, change exception message to uppercase

--- src/main/java/actions/json/CreateBookingJson.java
+++ src/main/java/actions/json/CreateBookingJson.java
@@ -18,7 +18,7 @@ public class CreateBookingJson {
                             String checkout,
                             String additionalneeds) {
         try {
-            response = Unirest.post(GlobalConstants.loadProd("url"))
+            response = Unirest.post(GlobalConstants.loadProperties("prod", "url"))
                     .contentType(GlobalConstants.JSON)
                     .accept(GlobalConstants.JSON)
                     .body(GlobalConstants.JSON(firstname,
@@ -32,29 +32,29 @@ public class CreateBookingJson {
             jsonObject = new JSONObject(response.getBody());
             System.out.println("POST: " + response.getBody());
         } catch (Exception exception) {
-            throw new RuntimeException("Send POST request fail");
+            throw new RuntimeException("SEND POST REQUEST FAIL");
         }
     }
 
     public void GetBookingId() {
         try {
             bookingId = jsonObject.get("bookingid").toString();
-            System.out.println("Booking ID: " + bookingId);
+            System.out.println("BOOKING ID: " + bookingId);
         } catch (Exception exception) {
-            throw new RuntimeException("Get booking id fail");
+            throw new RuntimeException("GET BOOKING ID FAIL");
         }
 
     }
 
-    public void ValidateStatusCode() {
-        int status;
+    public void ValidateStatusCode(String statusCode) {
+        String statusCodeResponse;
         try {
-            status = response.getStatus();
-            System.out.println("CODE: " + status + " " + response.getStatusText());
+            statusCodeResponse = String.valueOf(response.getStatus());
+            System.out.println("STATUS CODE: " + statusCodeResponse + " " + response.getStatusText());
         } catch (Exception exception) {
-            throw new RuntimeException("Cannot get status code");
+            throw new RuntimeException("GET STATUS CODE FAIL");
         }
-        GlobalConstants.softAssert.assertEquals(status, 200);
+        GlobalConstants.softAssert.assertEquals(statusCodeResponse, statusCode);
         GlobalConstants.softAssert.assertAll("Validate status code fail");
     }
 }
