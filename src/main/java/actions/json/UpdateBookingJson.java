312c291b438ed689857cf072f84588ac7d43062d
ntdsrd <thedat0309@gmail.com>
2024-02-22 22:47:09 +0700

    refactor: loadProd to loadProperties, exception message to uppercase

--- src/main/java/actions/json/UpdateBookingJson.java
+++ src/main/java/actions/json/UpdateBookingJson.java
@@ -12,18 +12,22 @@ public class UpdateBookingJson {
                            String checkin,
                            String checkout,
                            String additionalneeds) {
-        HttpResponse<String> response = Unirest.put(GlobalConstants.loadProd("url").concat("/" + CreateBookingJson.bookingId))
-                .contentType(GlobalConstants.JSON)
-                .accept(GlobalConstants.JSON)
-                .basicAuth(GlobalConstants.loadProd("username"), GlobalConstants.loadProd("password"))
-                .body(GlobalConstants.JSON(firstname,
-                        lastname,
-                        totalprice,
-                        depositpaid,
-                        checkin,
-                        checkout,
-                        additionalneeds))
-                .asString();
-        System.out.println("PUT: " + response.getBody());
+        try {
+            HttpResponse<String> response = Unirest.put(GlobalConstants.loadProperties("prod", "url").concat("/" + CreateBookingJson.bookingId))
+                    .contentType(GlobalConstants.JSON)
+                    .accept(GlobalConstants.JSON)
+                    .basicAuth(GlobalConstants.loadProperties("prod", "username"), GlobalConstants.loadProperties("prod", "password"))
+                    .body(GlobalConstants.JSON(firstname,
+                            lastname,
+                            totalprice,
+                            depositpaid,
+                            checkin,
+                            checkout,
+                            additionalneeds))
+                    .asString();
+            System.out.println("PUT: " + response.getBody());
+        } catch (Exception exception) {
+            throw new RuntimeException("Send PUT request fail");
+        }
     }
 }
