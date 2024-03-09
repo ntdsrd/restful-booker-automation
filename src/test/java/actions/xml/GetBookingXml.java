a58ccc12fef7a571e65bcb66db64213e04582635
ntdsrd <thedat0309@gmail.com>
2024-03-09 19:45:17 +0700

    feat: add validateFirstName method

--- src/test/java/actions/xml/GetBookingXml.java
+++ src/test/java/actions/xml/GetBookingXml.java
@@ -3,12 +3,24 @@ package actions.xml;
 import actions.commons.GlobalConstants;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.json.JSONObject;
+import org.json.XML;
 
 public class GetBookingXml {
+    JSONObject jsonObject;
+
     public void sendGetRequest() {
         HttpResponse<String> response = Unirest.get(GlobalConstants.loadProperties("Prod", "url").concat("/" + CreateBookingXml.bookingId))
                 .accept(GlobalConstants.XML_ACCEPT)
                 .asString();
+        jsonObject = XML.toJSONObject(response.getBody());
         System.out.println("Get: " + response.getBody());
+    }
+
+    public void validateFirstName(String firstName) {
+        String firstNameResponse = jsonObject.getJSONObject("booking").getString("firstname");
+        System.out.println("First name: " + firstNameResponse);
+        GlobalConstants.softAssert.assertEquals(firstNameResponse, firstName);
+        GlobalConstants.softAssert.assertAll("Validate first name fail");
     }
 }
