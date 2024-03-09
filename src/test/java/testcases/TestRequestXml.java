c80fda193ae4e08e8cc29839035690aef048a93a
ntdsrd <thedat0309@gmail.com>
2024-03-09 19:23:50 +0700

    test: add get booking id for testPostRequestXml, add testGetRequestXml

--- src/test/java/testcases/TestRequestXml.java
+++ src/test/java/testcases/TestRequestXml.java
@@ -1,14 +1,26 @@
 package testcases;
 
 import actions.xml.CreateBookingXml;
+import actions.xml.GetBookingXml;
 import org.testng.annotations.Test;
 
 public class TestRequestXml {
     CreateBookingXml createBookingXml = new CreateBookingXml();
+    GetBookingXml getBookingXml = new GetBookingXml();
 
     @Test(description = "Post: CreateBookingXml")
     public void testPostRequestXml() {
         //post request
         createBookingXml.sendPostRequest();
+        //get booking id
+        createBookingXml.getBookingId();
+        System.out.println("--------------------------------------------------");
+    }
+
+    @Test(description = "Get: GetBookingXml", dependsOnMethods = {"testPostRequestXml"})
+    public void testGetRequestXml() {
+        //get request
+        getBookingXml.sendGetRequest();
+        System.out.println("--------------------------------------------------");
     }
 }
