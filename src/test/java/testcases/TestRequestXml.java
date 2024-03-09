6f49db70552e28a94986af37eea9edb265c8616c
ntdsrd <thedat0309@gmail.com>
2024-03-09 19:32:20 +0700

    test: add put request for testPutRequestXml

--- src/test/java/testcases/TestRequestXml.java
+++ src/test/java/testcases/TestRequestXml.java
@@ -2,11 +2,13 @@ package testcases;
 
 import actions.xml.CreateBookingXml;
 import actions.xml.GetBookingXml;
+import actions.xml.UpdateBookingXml;
 import org.testng.annotations.Test;
 
 public class TestRequestXml {
     CreateBookingXml createBookingXml = new CreateBookingXml();
     GetBookingXml getBookingXml = new GetBookingXml();
+    UpdateBookingXml updateBookingXml = new UpdateBookingXml();
 
     @Test(description = "Post: CreateBookingXml")
     public void testPostRequestXml() {
@@ -21,6 +23,13 @@ public class TestRequestXml {
     public void testGetRequestXml() {
         //get request
         getBookingXml.sendGetRequest();
+        System.out.println("--------------------------------------------------");
+    }
+
+    @Test(description = "Put: UpdateBookingXml", dependsOnMethods = {"testPostRequestXml"})
+    public void testPutRequestXml() {
+        //put request
+        updateBookingXml.sendPutRequest();
         System.out.println("--------------------------------------------------");
     }
 }
