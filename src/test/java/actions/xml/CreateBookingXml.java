82fcc06d18fa5df9fb0ffcc24acfe5d2c9220f3b
ntdsrd <thedat0309@gmail.com>
2024-03-09 19:21:14 +0700

    feat: add getBookingId method

--- src/test/java/actions/xml/CreateBookingXml.java
+++ src/test/java/actions/xml/CreateBookingXml.java
@@ -3,8 +3,13 @@ package actions.xml;
 import actions.commons.GlobalConstants;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.json.JSONObject;
+import org.json.XML;
 
 public class CreateBookingXml {
+    public static String bookingId;
+    JSONObject jsonObject;
+
     public void sendPostRequest() {
         HttpResponse<String> response = Unirest.post(GlobalConstants.loadProperties("Prod", "url"))
                 .contentType(GlobalConstants.XML_CONTENT_TYPE)
@@ -17,6 +22,12 @@ public class CreateBookingXml {
                         GlobalConstants.loadProperties("Prod", "checkout"),
                         GlobalConstants.loadProperties("Prod", "additionalneeds")))
                 .asString();
+        jsonObject = XML.toJSONObject(response.getBody());
         System.out.println("Post: " + response.getBody());
+    }
+
+    public void getBookingId() {
+        bookingId = jsonObject.getJSONObject("created-booking").get("bookingid").toString();
+        System.out.println("Booking id: " + bookingId);
     }
 }
