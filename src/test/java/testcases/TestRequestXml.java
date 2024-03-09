9bb48734f3e7603e37224826986942fa3eab23ec
ntdsrd <thedat0309@gmail.com>
2024-03-09 18:22:32 +0700

    test: add post request for testPostRequestXml

---
+++ src/test/java/testcases/TestRequestXml.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import actions.xml.CreateBookingXml;
+import org.testng.annotations.Test;
+
+public class TestRequestXml {
+    CreateBookingXml createBookingXml = new CreateBookingXml();
+
+    @Test(description = "Post: CreateBookingXml")
+    public void testPostRequestXml() {
+        //post request
+        createBookingXml.postRequest();
+    }
+}
