9af4d667b75b616225c94fc18d42244941deeee7
ntdsrd <thedat0309@gmail.com>
2024-03-12 20:34:18 +0700

    feat: add validateForApiSchema method

--- src/test/java/actions/xml/UpdateBookingXml.java
+++ src/test/java/actions/xml/UpdateBookingXml.java
@@ -5,12 +5,25 @@ import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.json.JSONObject;
 import org.json.XML;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class UpdateBookingXml {
+    HttpResponse<String> response;
     JSONObject jsonObject;
 
     public void sendPutRequest() {
-        HttpResponse<String> response = Unirest.put(GlobalConstants.loadProperties("Prod", "url").concat("/" + CreateBookingXml.bookingId))
+        response = Unirest.put(GlobalConstants.loadProperties("Prod", "url").concat("/" + CreateBookingXml.bookingId))
                 .contentType(GlobalConstants.XML_CONTENT_TYPE)
                 .accept(GlobalConstants.XML_ACCEPT)
                 .basicAuth(GlobalConstants.loadProperties("Prod", "username"), GlobalConstants.loadProperties("Prod", "password"))
@@ -31,5 +44,25 @@ public class UpdateBookingXml {
         System.out.println("Information as needed: " + additionalNeedsResponse);
         GlobalConstants.softAssert.assertEquals(additionalNeedsResponse, additionalNeeds);
         GlobalConstants.softAssert.assertAll("Validate information as needed fail");
+    }
+
+    public void validateForApiSchema() {
+        Path path = Paths.get(GlobalConstants.XML_RESPONSE);
+        try {
+            if (Files.exists(path)) {
+                Files.delete(path);
+            } else {
+                Files.createFile(path);
+            }
+            Files.write(path, response.getBody().getBytes());
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File(GlobalConstants.xmlSchema("GetAndPut")));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(GlobalConstants.XML_RESPONSE));
+            System.out.println("Schema validated");
+            Files.delete(path);
+        } catch (IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
